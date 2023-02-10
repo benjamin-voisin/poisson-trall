51e9b65bc4cc513e81c7bfee61961b5b18a2037d
Vneb23 <benjamin.voisin@lilo.org>
2023-02-10 16:31:07 +0100

    world gen ça presque marche

--- world_gen.js
+++ world_gen.js
@@ -1,7 +1,17 @@
-function check_full_world(world){
+function get_voisins(world,cell){
+	let array = [];
+	if ((cell.j + 1 < world.width) && (world.grid[cell.i][cell.j + 1].is_free())) {array.push(world.grid[cell.i][cell.j + 1])};
+	if ((0 <= cell.j - 1) && (world.grid[cell.i][cell.j - 1].is_free())) {array.push(world.grid[cell.i][cell.j - 1])};
+	if ((cell.i + 1 < world.height) && (world.grid[cell.i + 1][cell.j].is_free())) {array.push(world.grid[cell.i + 1][cell.j])};
+	if ((0 <= cell.i - 1) && (world.grid[cell.i - 1][cell.j].is_free())) {array.push(world.grid[cell.i - 1][cell.j])};
+	return array;
+}
+
+
+function is_not_full(world){
 	for (let i = 0; i < world.width; i++){
-		for (let j = 0; j <world.height; j++){
-			if(world.grid[i][j].celltype === cell.empty){
+		for (let j = 0; j < world.height; j++){
+			if (world.grid[i][j].celltype === cell.empty){
 				return true
 			}
 		}
@@ -14,47 +24,54 @@ function create_arbitrary_path(world){
 	let rd = floor(random(n*n));
 	let i_depart = rd % n;
 	let j_depart = floor(rd / n);
-	while (world.grid[i_depart][j_depart].number !== null){
+	while (world.grid[i_depart][j_depart].celltype !== cell.empty){
 		let rd = floor(random(n*n));
 		let i_depart = rd % n;
 		let j_depart = floor(rd / n);
 	}
-	let voisins = get_neighbour(world.grid[i][j]);
+	let voisins = get_voisins(world, world.grid[i_depart][j_depart]);
 	//Si cette cellule n’a aucun voisin, paf on la transforme en mur
 	if (voisins.length == 0){
-		world.grid[i][j].wall = true;
+		world.grid[i_depart][j_depart].wall = true;
 	}
 	//Sinon, on prend des voisins randoms jusqu'à arriver à un point bloquant
 	//qui sera la fin de notre chemin.
 	else{
 		let path_id = world.path.length;
-		world.path.push(new Path(path_id, world.grid[i][j], world.grid[i][j]));
-		while(voisins.length != 0){
-			let new_cell = voisins[random(voisins.length)];
+		world.grid[i_depart][j_depart].path = true;
+		world.grid[i_depart][j_depart].number = path_id;
+		world.path.push(new Path(path_id, world.grid[i_depart][j_depart], world.grid[i_depart][j_depart]));
+		while(voisins.length > 2){
+			let r = floor(random(voisins.length));
+			new_cell = voisins[r];
+		new_cell.path = true;
 			world.path[path_id].tilelist.push(new_cell);
 			world.path[path_id].cellend = new_cell;
-			voisins = get_neighbour(new_cell);
+			voisins = get_voisins(world, new_cell);
 		}
+		new_cell.number = path_id;
 	}
 }
 
 function clear_paths(world){
 	for (let k = 0; k < world.path.length; k++){
-		world.path[k].tileliste = [];
+		world.path[k].tilelist = [];
 	}
 }
 
-function generate_map(height, width) {
+function generate_map(height, width, n) {
 	w = floor(width / n);
 	//On crée un monde vied
 	world = World.makeEmptyWorld(n,n,w);
 	//Tant que le monde n’est pas rempli, on ajoute des chemins (ou des murs)
 	//dedans
-	while (not check_full_world(world)){
-		create_arbitrary_path(world);
-	}
+	create_arbitrary_path(world);
+	// while (is_not_full(world)){
+	// 	create_arbitrary_path(world);
+	// }
 	//À la fin, on vide les chemins de tous les points intérieurs, pour en faire
 	//des targets
 	clear_paths(world);
+	console.log(world);
 	return world
 }
